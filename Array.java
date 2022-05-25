46f7ce6ff884485c774631c238526a1c3c5233b8
aufal02 <aufaluzumaki@gmail.com>
2022-05-25 20:51:50 +0700

    array

---
+++ Array.java
@@ -0,0 +1,12 @@
+public class Array {
+   String BilanganBulat[]; 
+   int [] BilanganBulat1;
+   
+   public void setBilanganBulat(String Bilanganbulat[]){
+       this.BilanganBulat=Bilanganbulat;
+    //    BilanganBulat=null;
+   }
+   public String [] getBilanganBulat(){
+       return BilanganBulat;
+   }
+}
---
+++ MainArray.java
@@ -0,0 +1,9 @@
+public class MainArray {
+   public static void main(String[] args) {
+
+    String [] BilanganBulat = {"aku","saya","dia","anu","hhh","klklk"};
+       Array ob = new Array();
+       ob.setBilanganBulat(BilanganBulat);
+       System.out.println("nama "+ob.getBilanganBulat()[0]);
+   } 
+}
